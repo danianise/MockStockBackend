c1a7b3221652abd2ab023ffd307b27315766b5bc
danianise <danianisehoey@gmail.com>
2023-04-04 10:44:42 -0400

    Change to try and fix form submission

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,12 @@ const mongoURI =
         ? process.env.DB_URL
         : 'mongodb+srv://danianise:danianise@cluster0.vg6ym.mongodb.net/test?authSource=admin&replicaSet=atlas-g3vm8g-shard-0&readPreference=primary&ssl=true';
 
-mongoose.connect(mongoURI)
+mongoose.connect(mongoURI,{
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+})
     .then((instance) =>
         console.log(`Connected to db: ${instance.connections[0].name}`)
     )
